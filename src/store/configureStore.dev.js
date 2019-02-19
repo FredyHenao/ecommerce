49fad7415ebc128eeab6dd3f7e69acf7b70491f0
FredyHenao <jhongallego45y@hotmail.com>
2019-02-19 18:30:28 -0500

    Actions Asíncronas

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import {createStore, applyMiddleware} from "redux";
 import logger from 'redux-logger';
-import { composeWithDevTools } from "redux-devtools-extension";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 import rootReducer from '../reducers';
 
 const enhacer = composeWithDevTools(
-  applyMiddleware(logger())
+    applyMiddleware(logger())
 );
 
 export default function configureStore(initialState) {
-    return createStore(rootReducer,initialState,enhacer)
+    return createStore(rootReducer, initialState, enhacer)
 }
